6af7da35f4679899bd772f5a570b154f62798048
E.V. Smith <emilies@bellevuearts.org>
2016-06-02 17:19:54 -0700

    dividing script file into two separate files

--- scripts/videography.js
+++ scripts/videography.js
@@ -1,80 +1,3 @@
-
-
-// -----------------------------------------------MY CODE ----------------------------------------------------------
-
-//Thumb 4 Caption Functions
-    if (screen.width >= 851) {
-      document.getElementById('thumb4').addEventListener('mouseover', displayCaption4);
-    }
-
-    if (screen.width >= 851) {
-      document.getElementById('thumb4').addEventListener('mouseout', displayThumb4);
-    }
-
-    function displayCaption4() {
-      document.getElementById('thumbCaption4').style.opacity="1";
-      document.getElementById('thumb4').innerHTML = '<img class="thumb" src="images/photography/2015-12_01-overlay.jpg">'; 
-    }
-
-    function displayThumb4() {
-      document.getElementById('thumb4').innerHTML = '<img class="thumb" src="images/photography/2015-12_01.jpg">'; 
-      document.getElementById('thumbCaption4').style.opacity="0";
-    }
-    
-    //Thumb 3 Caption Functions
-    if (screen.width >= 851) {
-      document.getElementById('thumb3').addEventListener('mouseover', displayCaption3);
-    }
-
-    if (screen.width >= 851) {
-      document.getElementById('thumb3').addEventListener('mouseout', displayThumb3);
-    }
-
-    function displayCaption3() {
-      document.getElementById('thumb3').innerHTML = '<img class="thumb" src="images/photography/halloween-2015-02-overlay.jpg">'; 
-      document.getElementById('thumbCaption3').style.opacity="1";
-    }
-    function displayThumb3() {
-      document.getElementById('thumb3').innerHTML = '<img class="thumb" src="images/photography/halloween-2015-02.jpg">'; 
-      document.getElementById('thumbCaption3').style.opacity="0";
-    }
-    
-    //Thumb 2 Caption Functions
-     if (screen.width >= 851) {
-      document.getElementById('thumb2').addEventListener('mouseover', displayCaption2);
-    }
-
-    if (screen.width >= 851) {
-      document.getElementById('thumb2').addEventListener('mouseout', displayThumb2);
-    }
-
-    function displayCaption2() {
-      document.getElementById('thumb2').innerHTML = '<img class="thumb" src="images/photography/2015-07-04_01-overlay.jpg">'; 
-      document.getElementById('thumbCaption2').style.opacity="1";
-    }
-    function displayThumb2() {
-      document.getElementById('thumb2').innerHTML = '<img class="thumb" src="images/photography/2015-07-04_01.JPG">'; 
-      document.getElementById('thumbCaption2').style.opacity="0";
-    }
-    
-    //Thumb 1 Caption Functions
-     if (screen.width >= 851) {
-      document.getElementById('thumb1').addEventListener('mouseover', displayCaption1);
-    }
-
-    if (screen.width >= 851) {
-      document.getElementById('thumb1').addEventListener('mouseout', displayThumb1);
-    }
-
-    function displayCaption1() {
-      document.getElementById('thumb1').innerHTML = '<img class="thumb" src="images/photography/oregon-coast-01-overlay.jpg">'; 
-      document.getElementById('thumbCaption1').style.opacity="1";
-    }
-    function displayThumb1() {
-      document.getElementById('thumb1').innerHTML = '<img class="thumb" src="images/photography/oregon-coast-01.JPG">'; 
-      document.getElementById('thumbCaption1').style.opacity="0";
-    }
-/*
 //code from http://blog.teamtreehouse.com/building-custom-controls-for-html5-videos
       // Video
       var video = document.getElementById("video");
@@ -106,9 +29,9 @@
 
     playButton.addEventListener("mouseover", function(){
           if (video.paused == true) {
-          playButton.innerHTML = '<i class="fa fa-pause-circle fa-5x" aria-hidden="true" id="play-pause"></i>';
-          } else {
           playButton.innerHTML = '<i class="fa fa-play-circle fa-5x" aria-hidden="true" id="play-pause"></i>';
+          } else {
+          playButton.innerHTML = '<i class="fa fa-pause-circle fa-5x" aria-hidden="true" id="play-pause"></i>';
          }
         });
   
@@ -185,4 +108,3 @@
       // Update the video volume
       video.volume = volumeBar.value;
     });
-*/
